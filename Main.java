6021375535e1bd17e2b70e1e255deea31c3d5fd6
AnderEder <135076154+AnderEder@users.noreply.github.com>
2024-12-02 17:21:21 -0500

    Verificando funcionamiento del compilador

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
+        int fechaLanzamiento = 2000;
+
         String sinopsis = """
                 Es una pelicula muy buena por parte de los directores
-                """;
+                """ + fechaLanzamiento;
 
         System.out.println(sinopsis);
+
+        for (int i = 0; i < fechaLanzamiento ; i++) {
+            System.out.println(i + " ");
+        }
+
     }
 }
 
