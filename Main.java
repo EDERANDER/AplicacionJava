d6d6155f34f6d7135a8c5b16b68de634bdc177c7
AnderEder <135076154+AnderEder@users.noreply.github.com>
2024-12-02 17:17:13 -0500

    Creando el proyecto para guardarlo en GitHub

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String sinopsis = """
+                Es una pelicula muy buena por parte de los directores
+                """;
+
+        System.out.println(sinopsis);
+    }
+}
+
