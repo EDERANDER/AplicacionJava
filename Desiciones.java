986f2e6ff709cb35a8ab4fddc71ebc1ad4d60abb
AnderEder <135076154+AnderEder@users.noreply.github.com>
2024-12-02 18:24:17 -0500

    Archivos que no valen la pena

---
+++ Desiciones.java
@@ -0,0 +1,18 @@
+public class Desiciones {
+    public static void main(String[] args) {
+        int expresion = 2;
+
+        switch (expresion) {
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+
+            default:
+                System.out.println("Opcion que no se sabe");
+
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -3,15 +3,19 @@ public class Main {
 
         int fechaLanzamiento = 2000;
 
+        double media = (8.2 + 6.0 + 4.4) / 3;
+
         String sinopsis = """
                 Es una pelicula muy buena por parte de los directores
                 """ + fechaLanzamiento;
 
         System.out.println(sinopsis);
 
-        for (int i = 0; i < fechaLanzamiento ; i++) {
-            System.out.println(i + " ");
-        }
+        int clasficacion = (int) (media / 2);
+
+        System.out.println(clasficacion);
+
+
 
     }
 }
